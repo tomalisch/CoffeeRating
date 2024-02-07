60b810ca793ad644fe3ce803ecde684fb1a40f78
Tom Alisch <alisch.tom@web.de>
2024-02-07 15:53:18 -0500

    Initial Commit

--- CoffeeRatings.py
+++ CoffeeRatings.py
@@ -1,1 +1,11 @@
-import mysql-connector-python
+## Dependencies
+import mysql.connector
+##
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="yourpassword"
+)
+
+print(mydb)
