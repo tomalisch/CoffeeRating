5c32397ca8a4a3f10b644c5f51edbc285d444bd5
Tom Alisch <alisch.tom@web.de>
2024-02-08 14:49:31 -0500

    Added new table query and plotting rating on price

--- CoffeeRatings.py
+++ CoffeeRatings.py
@@ -16,12 +16,22 @@ mydb = mysql.connector.connect(
 # Create dataframe of all entries and close mySQL database connection
 query = "Select * FROM CoffeeRatings;"
 dataCR = pd.read_sql(query, mydb)
+query = "Select * FROM CoffeeStats;"
+dataCS = pd.read_sql(query, mydb)
 mydb.close()
 
-# Bargraph of data
+# Bargraph of ratings on coffee, rater as legend
+plt.figure()
 ax = sns.barplot(dataCR, x='CoffeeName', y='Rating',facecolor='gray')
 sns.stripplot(dataCR, x='CoffeeName', y='Rating', hue='RaterName')
 handles, labels = ax.get_legend_handles_labels()
 ax.legend( title='Rater', bbox_to_anchor=(1, 1.02), loc='upper left')
 plt.setp( ax.xaxis.get_majorticklabels(), rotation=45, ha='right', rotation_mode='anchor' );
+
+# Scatter plot of mean Rating on Price (per 2lb), dots scaled by number of individual ratings
+plt.figure()
+ax = sns.scatterplot(dataCS, x='Price', y='mRating', s=np.array(dataCS.nRatings)*50,hue='CoffeeName',legend='brief')
+handles, labels = ax.get_legend_handles_labels()
+ax.legend( title='Coffee', bbox_to_anchor=(1, 1.02), loc='upper left');
+
 
