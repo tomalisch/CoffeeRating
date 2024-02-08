81d04f01350693875066c7dc2b20a7ad424a90c5
Tom Alisch <alisch.tom@web.de>
2024-02-08 16:51:38 -0500

    Added Roast and Origin columns

--- CoffeeRatings.py
+++ CoffeeRatings.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 ##
 
-# Connect to mySQL database
+# Connect to mySQL deBivort Lab Coffee database
 mydb = mysql.connector.connect(
   host="localhost",
   user="root",
@@ -22,13 +22,17 @@ mydb.close()
 
 # Create derived dataframe suitable for analyses and visualizations
 data = dataCR.copy()
-# Create new Price column 
+# Create new Price, Roast, Origin columns
 data['Price'] = np.NaN
-# Loop through new dataframe rows and populate Price column by querying coffee price from CoffeeStats database
+data['Roast'] = np.NaN
+data['Origin'] = np.NaN
+# Loop through new dataframe rows and populate Price, Roast, and Origin columns by querying corresponding entries from CoffeeStats
 for i, name in enumerate(data['CoffeeName']):
     data['Price'].iloc[i] = np.array(dataCS['Price'][dataCS['CoffeeName']==name]).copy()
+    data['Roast'].iloc[i] = np.array(dataCS['Roast'][dataCS['CoffeeName']==name]).copy()
+    data['Origin'].iloc[i] = np.array(dataCS['Origin'][dataCS['CoffeeName']==name]).copy()
 
-# Bargraph of ratings on coffee, rater as legend
+# Bargraph of Rating per coffee, rater as legend
 plt.figure()
 ax = sns.barplot(dataCR, x='CoffeeName', y='Rating',facecolor='gray')
 sns.stripplot(dataCR, x='CoffeeName', y='Rating', hue='RaterName')
@@ -38,7 +42,7 @@ plt.setp( ax.xaxis.get_majorticklabels(), rotation=45, ha='right', rotation_mode
 
 # Scatter plot of mean Rating on Price (per 2lb), dots scaled by number of individual ratings
 plt.figure()
-ax = sns.scatterplot(dataCS, x='Price', y='mRating', s=np.array(dataCS.nRatings)*50,hue='CoffeeName',legend='brief')
+ax = sns.scatterplot(dataCS, x='Price', y='mRating', s=np.array(dataCS.nRatings)*75,hue='CoffeeName',legend='brief')
 handles, labels = ax.get_legend_handles_labels()
 ax.legend( title='Coffee', bbox_to_anchor=(1, 1.02), loc='upper left');
-ax = sns.regplot(x='Price', y='Rating',data=a, order=2,scatter=False)
+ax = sns.regplot(x='Price', y='Rating',data=data, order=2,scatter=False)
